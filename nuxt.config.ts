e851caf7e8f1d768ae82ef01d68ec2f48108f150
Daniel A. Esteban <daniel@datak.co>
2024-05-29 10:19:22 -0500

    Add persistence to pinia/vuex store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
-  modules: ["@nuxt/ui", '@pinia/nuxt', '@nuxt/test-utils/module'],
+  modules: ["@nuxt/ui", '@pinia/nuxt', '@nuxt/test-utils/module', '@pinia-plugin-persistedstate/nuxt'],
   ui: {
     icons: ["heroicons", "ri"],
   }
--- stores/main.ts
+++ stores/main.ts
@@ -1,16 +1,24 @@
-const LOCAL_STORAGE_KEY = 'ac_'
+const LOCAL_STORAGE_KEY = 'assessment'
 
 export const useMainStore = defineStore('main', () => {
     const selectedProduct = ref({} as Product)
-    const creditCardInfo = ref({} as CreditCardInfo)
+    const creditCardInfo = ref({
+        expirationDate: {
+            month: '',
+            year: ''
+        },
+        cardHolder: '',
+        cardNumber: '',
+        cvv: ''
+    } as CreditCardInfo)
 
-    if (process.client)
-        selectedProduct.value = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
-
-    watch(selectedProduct, () => {
-        if (process.client)
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(selectedProduct.value))
+    const validationResult = computed(() => {
+        if (creditCardInfo.value.cardNumber && creditCardInfo.value.cardNumber.length >= 13)
+            return validateCreditCard(creditCardInfo.value.cardNumber)
+        return null
     })
 
-    return { selectedProduct, creditCardInfo }
+    return { selectedProduct, creditCardInfo, validationResult }
+}, {
+    persist: true
 })
